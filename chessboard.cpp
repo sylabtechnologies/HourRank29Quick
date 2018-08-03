85c2d733a3c77974336cb2424906c044357cd637
sylabtechnologies <32247027+sylabtechnologies@users.noreply.github.com>
2018-08-03 16:33:38 -0400

    0st iteration

--- chessboard.cpp
+++ chessboard.cpp
@@ -5,9 +5,9 @@ inline string alpha_result(bool result) { return result ? "Yes" : "No"; }
 bool test_adjacents(const vector<vector<int>> &board, const size_t testsize, const size_t i, const size_t j, const int color)
 {
 	if (j > 0 && board[i][j - 1] == color) return false;
-	if (j - 1 < testsize && board[i][j + 1] == color) return false;
+	if (j + 1 < testsize && board[i][j + 1] == color) return false;
 	if (i > 0 && board[i - 1][j] == color) return false;
-	if (i - 1 < board.size() && board[i + 1][j] == color) return false;
+	if (i + 1 < board.size() && board[i + 1][j] == color) return false;
 
 	return true;
 }
