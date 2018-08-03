6315d40a55aff693cdc59d502cf1c72537d2c6dd
sylabtechnologies <32247027+sylabtechnologies@users.noreply.github.com>
2018-08-03 16:52:33 -0400

    0st iter
    Lookup permutations, use map?

---
+++ arraypartition.cpp
@@ -0,0 +1,25 @@
+#define MY_MAX_NUM  1000001
+typedef vector<int> Ivec;
+
+int solve(Ivec a)
+{
+	// get primes
+	Ivec primes = get_primes(MY_MAX_NUM);
+
+	// get divisors
+	vector<Ivec> divisors;
+	for (size_t i = 0; i < a.size(); i++)
+	{
+		Ivec mydivisors = get_divisors(a[i], primes);
+		divisors.push_back(mydivisors);
+	}
+
+	// count unique divisors
+	for (size_t i = 0; i < divisors.size(); i++)
+	{
+
+	}
+
+	// compute the answer per permutation formulas
+	return ();
+}
